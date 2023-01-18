f0202c3fb370dd009d254a297f44974576be8999
Ivan Marchuk <ivan.marchuk@mail.ru>
2023-01-18 14:29:03 +0300

    test: tested with chrome tools

--- server/server.go
+++ server/server.go
@@ -3,32 +3,47 @@ package server
 import (
 	"fmt"
 	"io"
-	"net"
 
 	"golang.org/x/net/websocket"
 )
 
 type Server struct {
-	known       []*net.Addr
+	connCounter int
 	activeConns map[*websocket.Conn]bool
+	connch      chan *websocket.Conn
 }
 
 func NewServer() *Server {
-	return &Server{
+	server := &Server{
 		activeConns: make(map[*websocket.Conn]bool),
+		connch:      make(chan *websocket.Conn, 10),
 	}
+
+	go server.RunConnectionLoop()
+
+	return server
 }
 
 func (s *Server) HandleConn(ws *websocket.Conn) {
 	fmt.Println("New incoming conn from: ", ws.RemoteAddr())
-
-	// lock actor
-	s.activeConns[ws] = true
-
+	s.connch <- ws
 	s.readLoop(ws)
 }
 
+func (s *Server) RunConnectionLoop() {
+	for conn := range s.connch {
+		s.addConnection(conn)
+	}
+}
+
+func (s *Server) addConnection(conn *websocket.Conn) {
+	fmt.Println("get conn from connch ", conn.RemoteAddr())
+	s.activeConns[conn] = true
+	s.connCounter += 1
+}
+
 func (s *Server) readLoop(ws *websocket.Conn) {
+	fmt.Println("Reading from: ", ws.RemoteAddr())
 	buf := make([]byte, 1024)
 	for {
 		n, err := ws.Read(buf)
@@ -38,12 +53,12 @@ func (s *Server) readLoop(ws *websocket.Conn) {
 				break
 			}
 			fmt.Println("Read error: ", err)
-			continue
+			break
 		}
 		msg := buf[:n]
 		fmt.Println(string(msg))
 
-		ws.Write([]byte("msg received"))
+		ws.Write([]byte(fmt.Sprintf("msg received %d", s.connCounter)))
 	}
 }
 
